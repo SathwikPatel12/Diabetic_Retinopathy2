4a413609b9646a0d223fc0f183e2e262da2ea793
Sathwik_Patel <sathwikyalagala@gmail.com>
2025-07-30 14:20:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def load_lottie_url(url):
     return r.json()
 
 # Load a medical animation
-lottie_medical = load_lottie_url("https://assets4.lottiefiles.com/packages/lf20_ldfliwlm.json")
+lottie_medical = load_lottie_url("https://assets9.lottiefiles.com/packages/lf20_F9A4lW.json")
 
 # Display the animation
 if lottie_medical:
